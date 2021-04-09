9a326a1383ef1a288ef5f8985efd95679a233411
nitesh9001 <niteshsingh9001@gmail.com>
2021-04-09 18:30:57 +0530

    messsage

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const user = require('./Routes/userApis')
 const admin=require('./Routes/adminApis')
 require('dotenv').config()
 
-const port = process.env.PORT ;
+const port = process.env.PORT || 5000 ;
 const server = http.createServer(app);
 app.use(cors());
 // app.use(bodyParser.json())
