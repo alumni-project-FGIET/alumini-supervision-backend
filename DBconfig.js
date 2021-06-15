6719667b850e54daa42b29098f61b3504fb692d6
Nitesh kumar singh <niteshsingh9001@gmail.com>
2021-06-16 00:08:53 +0530

    Update DBconfig.js

--- DBconfig.js
+++ DBconfig.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 process.env.SUPPRESS_NO_CONFIG_WARNING = "Done";
 
 const connection = async () => {
-  const conn = await mongoose.connect(process.env.DBURL, {
+  const conn = await mongoose.connect("mongodb+srv://user:Alumni2021@alumni.ctgbh.mongodb.net/myAlumni", {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
